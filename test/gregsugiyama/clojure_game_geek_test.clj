845018d097d9e45c0b99e168d84f97151be51afa
gregsugiyama <gjsugiyama@gmail.com>
2023-07-07 12:33:00 -0700

    first commit

---
+++ test/gregsugiyama/clojure_game_geek_test.clj
@@ -0,0 +1,7 @@
+(ns gregsugiyama.clojure-game-geek-test
+  (:require [clojure.test :refer :all]
+            [gregsugiyama.clojure-game-geek :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
